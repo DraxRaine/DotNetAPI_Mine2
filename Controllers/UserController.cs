e313737aff83214647870c529d7be631db868693
Luchiano Fabiano <rxs077@gmail.com>
2023-12-08 15:33:54 -0500

    Updated launch file

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,10 +6,18 @@ namespace DotnetAPI.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
+    DataContextDapper _dapper;
 
     public UserController(IConfiguration config)
     {
-        Console.WriteLine(config.GetConnectionString("DefaultConnection"));
+        _dapper = new DataContextDapper(config);
+    }
+
+    [HttpGet("TestConnection")]
+
+    public DateTime TestConnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>("Select getdate()");
     }
 
     [HttpGet("GetUsers/{testValue}")]
